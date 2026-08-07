019a6d51e44eee2facab51c7e14e61e484308394
113062133 <csgaun@gmail.com>
2026-08-07 22:25:59 +0800

    Add: count-connected-components - submission-5

---
+++ Data Structures & Algorithms/count-connected-components/submission-5.py
@@ -0,0 +1,26 @@
+class DSU:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+
+    def find(self, node):
+        if self.parent[node] == node:
+            return node
+        self.parent[node] = self.find(self.parent[node])
+        return self.parent[node]
+
+    def union(self, node1, node2):
+        root1 = self.find(node1)
+        root2 = self.find(node2)
+        if root1 != root2:
+            self.parent[root1] = root2
+            return True
+        return False
+
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        num = n
+        dsu = DSU(n)
+        for a, b in edges:
+            if dsu.union(a, b):
+                num -= 1
+        return num
